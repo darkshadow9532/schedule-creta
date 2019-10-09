c301fcec3556c2d8861dc3c68ca10b8b281c13fa
darkshadow9532 <darkshadow9532@gmail.com>
2019-10-09 17:43:24 +0700

    add MongoDB

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: 'mongodb+srv://dark_shadow9532:<minh9532>@cluster0-auyfy.mongodb.net/test?retryWrites=true&w=majority'
+    url: 'mongodb+srv://dark_shadow9532:minh9532@cluster0-auyfy.mongodb.net/test?retryWrites=true&w=majority'
 }
